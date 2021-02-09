6dd08e562507ae7de9565b239472df72110f416d
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-02-09 22:57:34 +0700

    Init project

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;       
+    public float restartDelay = 5f;            
+
+
+    Animator anim;                          
+    float restartTimer;                    
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
