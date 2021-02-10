e216ce06ce56894f8fe345140fd1a0885617a5ce
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-02-10 21:29:36 +0700

    Implement player movement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,63 @@
 ﻿using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 6f;
+    public LayerMask floorMask;
     
+    private Vector3 direction;
+    private Animator anim;
+    private Rigidbody rb;
+    private float camRayLength = 100f;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        direction.Set(h, 0f, v);
+    }
+
+    private void FixedUpdate()
+    {
+        Move(direction);
+        Turning();
+        Animating(direction);
+    }
+
+    private void Move(Vector3 direction)
+    {
+        Vector3 movement = direction.normalized * speed * Time.fixedDeltaTime;
+        rb.MovePosition(transform.position + movement);
+    }
+
+    private void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0;
+
+            Quaternion rotation = Quaternion.LookRotation(playerToMouse);
+            rb.MoveRotation(rotation);
+        }
+    }
+
+    private void Animating(Vector3 direction)
+    {
+        bool isWalking = direction != Vector3.zero;
+        anim.SetBool("IsWalking", isWalking);
+    }
 }
