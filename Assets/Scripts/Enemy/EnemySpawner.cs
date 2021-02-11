e384ff6b073af2983101a4b617cce5e67eca469d
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-02-12 00:51:57 +0700

    Implement better enemy spawner

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,16 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject[] enemy;
+    [System.Serializable]
+    public struct EnemyType
+    {
+        public GameObject gameObject;
+        public float chanceToSpawn;
+    }
+
+    [SerializeField]
+    public EnemyType[] enemyTypes;
     public float firstDelay = 2f;
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
 
+    private float[] range;
+
     private void Start()
     {
+        range = new float[enemyTypes.Length];
+        for (int i = 0; i < range.Length; i++)
+        {
+            if (i == 0)
+            {
+                range[i] = enemyTypes[i].chanceToSpawn;
+            }
+            else
+            {
+                range[i] = enemyTypes[i].chanceToSpawn + range[i - 1];
+            }
+        }
+
         InvokeRepeating(nameof(Spawn), firstDelay, spawnTime);
     }
 
@@ -22,8 +43,19 @@ public class EnemySpawner : MonoBehaviour
             return;
         }
 
-        int enemyIndex = Random.Range(0, enemy.Length);
+        float enemyRandomRange = Random.Range(0, range[range.Length - 1]);
+        int enemyIndex = range.Length - 1;
+
+        for (int i = 0; i < range.Length; i++)
+        {
+            if (enemyRandomRange < range[i])
+            {
+                enemyIndex = i;
+                break;
+            }
+        }
+        Debug.Log(enemyTypes[enemyIndex].gameObject.name);
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-        Instantiate(enemy[enemyIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        Instantiate(enemyTypes[enemyIndex].gameObject, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
     }
 }
