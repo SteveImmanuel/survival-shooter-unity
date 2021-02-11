de1f47377fc76b97898952123811b990dd31895b
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-02-11 23:09:59 +0700

    Separate gun vfx and input handler script

---
+++ Assets/Scripts/Player/Gun.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+    public float range = 100f;
+
+    private ParticleSystem gunParticles;
+    private LineRenderer bulletTrail;
+    private Light gunLight;
+    private float elapsedTime;
+    private Ray shootRay;
+    private RaycastHit shootHit;
+    private AudioSource gunAudio;
+    private float effectsDisplayTime = 0.2f;
+
+    void Awake()
+    {
+        gunParticles = GetComponentInChildren<ParticleSystem>();
+        bulletTrail = GetComponentInChildren<LineRenderer>();
+        gunLight = GetComponentInChildren<Light>();
+        gunAudio = GetComponent<AudioSource>();
+    }
+
+    public void DisableEffects()
+    {
+        bulletTrail.enabled = false;
+        gunLight.enabled = false;
+    }
+
+    public void ShowEffect(Vector3 shootOrigin, Vector3 shootHit, Vector3 direction)
+    {
+        gunAudio.Play();
+        gunLight.enabled = true;
+
+        gunParticles.Stop();
+        gunParticles.Play();
+
+        if (shootHit == Vector3.positiveInfinity)
+        {
+            bulletTrail.SetPosition(1, shootOrigin + direction * range);
+        }
+        else
+        {
+            bulletTrail.SetPosition(1, shootHit);
+        }
+        bulletTrail.SetPosition(0, shootOrigin);
+        bulletTrail.enabled = true;
+
+        Invoke(nameof(DisableEffects), effectsDisplayTime);
+    }
+}
--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -2,66 +2,38 @@
 
 public class PlayerShooting : MonoBehaviour
 {
-    public int damagePerShot = 20;                  
-    public float timeBetweenBullets = 0.15f;        
-    public float range = 100f;                      
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+    public float range = 100f;
+    public Transform gunPoint;
+    public LayerMask shootableMask;
 
-    float timer;                                    
-    Ray shootRay;                                   
-    RaycastHit shootHit;                            
-    int shootableMask;                             
-    ParticleSystem gunParticles;                    
-    LineRenderer gunLine;                           
-    AudioSource gunAudio;                           
-    Light gunLight;                                 
-    float effectsDisplayTime = 0.2f;                
+    private Gun gun;
+    private float elapsedTime = 0f;
+    private Ray shootRay;
+    private RaycastHit shootHit;
 
-    void Awake()
+    private void Start()
     {
-        shootableMask = LayerMask.GetMask("Shootable");
-        gunParticles = GetComponent<ParticleSystem>();
-        gunLine = GetComponent<LineRenderer>();
-        gunAudio = GetComponent<AudioSource>();
-        gunLight = GetComponent<Light>();
+        gun = GetComponentInChildren<Gun>();
     }
 
     void Update()
     {
-        timer += Time.deltaTime;
+        elapsedTime += Time.deltaTime;
 
-        if (Input.GetButton("Fire1") && timer >= timeBetweenBullets)
+        if (Input.GetButton("Fire1") && elapsedTime >= timeBetweenBullets)
         {
             Shoot();
         }
-
-        if (timer >= timeBetweenBullets * effectsDisplayTime)
-        {
-            DisableEffects();
-        }
-    }
-
-    public void DisableEffects()
-    {
-        gunLine.enabled = false;
-        gunLight.enabled = false;
     }
 
     void Shoot()
     {
-        timer = 0f;
+        elapsedTime = 0f;
 
-        gunAudio.Play();
-
-        gunLight.enabled = true;
-
-        gunParticles.Stop();
-        gunParticles.Play();
-
-        gunLine.enabled = true;
-        gunLine.SetPosition(0, transform.position);
-
-        shootRay.origin = transform.position;
-        shootRay.direction = transform.forward;
+        shootRay.origin = gunPoint.position;
+        shootRay.direction = gunPoint.forward;
 
         if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
         {
@@ -71,12 +43,12 @@ public class PlayerShooting : MonoBehaviour
             {
                 enemyHealth.TakeDamage(damagePerShot, shootHit.point);
             }
-
-            gunLine.SetPosition(1, shootHit.point);
+            gun.ShowEffect(gunPoint.position, shootHit.point, Vector3.positiveInfinity);
         }
         else
         {
-            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+            gun.ShowEffect(gunPoint.position, Vector3.positiveInfinity, shootRay.direction);
+
         }
     }
 }
