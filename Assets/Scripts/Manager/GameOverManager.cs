e188c67b1aec433ec108922daad61bd8fa5b2ed5
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-02-12 14:35:14 +0700

    Reorganize structure

---
+++ Assets/Scripts/Manager/GameOverManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Animator))]
+public class GameOverManager : MonoBehaviour
+{
+    public float restartDelay = 5f;            
+
+    private Animator anim;                          
+    private bool isGameOver = false;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (PlayerHealth.currentHealth <= 0 && !isGameOver)
+        {
+            isGameOver = true;
+            anim.SetTrigger("GameOver");
+            Invoke(nameof(RestartGame), restartDelay);
+        }
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
