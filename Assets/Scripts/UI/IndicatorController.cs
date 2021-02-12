bcc64f7844064f80b4684b6dce306f78bda47728
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-02-12 14:36:37 +0700

    Separate ui code

---
+++ Assets/Scripts/UI/IndicatorController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animator))]
+public class IndicatorController : MonoBehaviour
+{
+    public float castRadius;
+    public float timeBetweenCast;
+    public GameObject arrow;
+    public Text enemyDistanceText;
+    public LayerMask detectableMask;
+
+    private Quaternion originalRotation;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        originalRotation = transform.rotation;
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(GetNearbyEnemy), 0, timeBetweenCast);
+    }
+
+    private void Update()
+    {
+        transform.rotation = originalRotation;
+    }
+
+    private void GetNearbyEnemy()
+    {
+        Collider[] enemies = Physics.OverlapSphere(transform.position, castRadius, detectableMask);
+        if (enemies.Length <= 0)
+        {
+            animator.SetBool("ShowIndicator", false);
+            return;
+        }
+
+        animator.SetBool("ShowIndicator", true);
+        float closestDistance = Mathf.Infinity;
+        Vector3 targetDirection = Vector3.zero;
+
+        foreach (Collider enemy in enemies)
+        {
+            Vector3 enemyPosition = enemy.transform.position;
+            enemyPosition.y = 0;
+
+            Vector3 direction = (enemyPosition - transform.position);
+            float sqrDistance = direction.sqrMagnitude;
+            
+            if (sqrDistance < closestDistance)
+            {
+                closestDistance = sqrDistance;
+                targetDirection = direction;
+            }
+        }
+
+        arrow.transform.rotation = Quaternion.LookRotation(targetDirection);
+        enemyDistanceText.text = string.Format("{0:0.00} m", closestDistance / 10);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, castRadius);
+    }
+}
---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Text scoreText;
+    public Slider healthSlider;
+
+    public static UIController instance;
+    private int currentScore = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void AddScore(int score)
+    {
+        currentScore += score;
+        scoreText.text = "Score: " + currentScore;
+    }
+
+    public void SetHealthSlider(float value)
+    {
+        healthSlider.value = value;
+    }
+}
